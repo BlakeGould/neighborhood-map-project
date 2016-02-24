fd22d3151fa56faf2a4a2d871a27ef9a2e6351ac
Blake Gould <gouldbf@gmail.com>
2016-02-24 00:05:28 -0700

    Update map center and add first marker.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 //call for a map of Colorado Springs and add it to the DOM
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 38.8756007, lng: -104.8984441},
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 38.83, lng: -104.825},
       zoom: 12
+  });
+  var marker = new google.maps.Marker({
+    position: {lat: 38.83, lng: -104.825},
+    map: map,
+    title: 'Colorado Springs'
   });
 }
 

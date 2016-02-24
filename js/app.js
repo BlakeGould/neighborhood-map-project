37e1377c917dd5f06ca07b0a17fe5a510d789aa2
Blake Gould <gouldbf@gmail.com>
2016-02-23 23:50:19 -0700

    Update map to show Colorado Springs instead of Australia.

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+      center: {lat: 38.8756007, lng: -104.8984441},
+      zoom: 12
   });
 }
 

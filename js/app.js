c04fcec6681aa71afe2723dcab54d73f2385c51f
Blake Gould <gouldbf@gmail.com>
2016-02-24 21:07:14 -0700

    "Create For loop to iterate over places object."

--- js/app.js
+++ js/app.js
@@ -5,11 +5,59 @@ function initMap() {
       center: {lat: 38.83, lng: -104.825},
       zoom: 12
   });
-  var marker = new google.maps.Marker({
-    position: {lat: 38.83, lng: -104.825},
-    map: map,
-    title: 'Colorado Springs'
-  });
+
+  var Marker = function() {
+    new google.maps.Marker({
+      position: {lat: 38.83, lng: -104.825},
+      map: map,
+      title: 'Colorado Springs'
+    });
+  };
+
+  var places = {
+    // name: 'cos'
+
+    place1 : {
+      stuff: 'things',
+      garbage: 'man',
+      position: {lat: 38.83, lng: -104.825},
+      map: map,
+      title: 'Colorado Springs'
+      }
+
+    // {position:  {lat: 38.83, lng: -104.825}, title: 'Colorado Springs'}
+  }
+
+    // for (var place in places) {
+    //   new Marker();
+    // }
+
+
+  // need to find a way to access properties of each item in the places object via this for loop...
+  for (var place in places) {
+    new google.maps.Marker({
+      position: {lat: places.place1.position.lat, lng: places.place1.position.lng},
+      map: places.place1.map,
+      title: places.place1.title
+    });
+    console.log(place.title)
+  }
+
+  // for (var place in places) {
+  //   new google.maps.Marker({
+  //     position: place.position,
+  //     map: place.map,
+  //     title: place.title
+  //   });
+  // }
+
+  // var allMarkers = [new Marker()];
+
+  // var marker = new google.maps.Marker({
+  //   position: {lat: 38.83, lng: -104.825},
+  //   map: map,
+  //   title: 'Colorado Springs'
+  // });
 }
 
 // function showLink() {
